1f4a9b121498e38007ad54b149adda0101185498
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-08 23:00:35 +0900

    Add: I've got it in working order.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-# pythonのバージョンは任意
 FROM python:3.11.5
 
 WORKDIR /app
--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 # 簡単なindexページの作成を行っています。
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "index page"
-
-# WARN: これを開発する時は、ryamaを閉じてください。 (docker-compose の contianer-name と port が 競合するため。)
-# TODO: あと tailwind css を入れてさっさと開発しよう。
+    return render_template("index.html")
