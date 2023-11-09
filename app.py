8cca4e5cab003c04a6b86b9a13cf24590ad9ed94
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-09 13:30:40 +0900

    Add: sqlalechemy and db migrate

--- app.py
+++
@@ -1,9 +0,0 @@
-# 簡単なindexページの作成を行っています。
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
---
+++ app/app.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}".format(
+    user=os.environ.get("POSTGRES_USER"),
+    passwd=os.environ.get("POSTGRES_PASSWORD"),
+    host=os.environ.get("POSTGRES_HOST"),
+    port=os.environ.get("POSTGRES_PORT"),
+    db=os.environ.get("POSTGRES_DB"),
+)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class students(db.Model):
+    id = db.Column("student_id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    addr = db.Column(db.String(200))
+
+    def __init__(self, name, city, addr):
+        self.name = name
+        self.city = city
+        self.addr = addr
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
