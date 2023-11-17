9645df44fcfd236da5f21ecdbc4ac52f88747c3e
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2023-11-17 10:30:58 +0900

    Change: login.html  Design

--- app/app.py
+++ app/app.py
@@ -86,5 +86,9 @@ class Comment(db.Model):
 def index():
     return render_template("index.html")
 
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
